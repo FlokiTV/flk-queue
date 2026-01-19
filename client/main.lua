c2925d28836499d15602a67a4cbab4cce1d17f99
FlokiTV <dawysonw@gmail.com>
2026-01-19 08:26:08 -0300

    feat: add givepistol command and improve text rendering readability

--- client/main.lua
+++ client/main.lua
@@ -22,9 +22,16 @@ RegisterCommand('status', function()
         Citizen.CreateThread(function()
             while showStatus do
                 Citizen.Wait(5)
-                DrawTextCustom(0.01, 0.35, 'Queue: ' .. statusData.queueCount, 0.5, { r = 255, g = 255, b = 255, a = 255 }, 4, false)
-                DrawTextCustom(0.01, 0.38, 'Matches: ' .. statusData.matchesCount, 0.5, { r = 255, g = 255, b = 255, a = 255 }, 4, false)
+                DrawTextCustom(0.01, 0.35, 'Queue: ' .. statusData.queueCount, 0.5,
+                    { r = 255, g = 255, b = 255, a = 255 }, 4, false)
+                DrawTextCustom(0.01, 0.38, 'Matches: ' .. statusData.matchesCount, 0.5,
+                    { r = 255, g = 255, b = 255, a = 255 }, 4, false)
             end
         end)
     end
+end, false)
+
+RegisterCommand("givepistol", function(source, args, rawCommand)
+    local myselfPed = PlayerPedId()
+    GiveWeaponToPed(myselfPed, GetHashKey('WEAPON_PISTOL'), 100, false, true)
 end, false)
