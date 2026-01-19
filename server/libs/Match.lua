9e6fd3cc50199b85954380b9f640353ad0c59d7c
FlokiTV <dawysonw@gmail.com>
2026-01-18 23:06:27 -0300

    feat(match): implement player data preservation in matches

--- server/libs/Match.lua
+++ server/libs/Match.lua
@@ -10,13 +10,22 @@ function Match.new(players)
     local self = setmetatable({}, Match)
     self.players = players
     self.matchId = #matches + 1
+    self.playerData = {}
     table.insert(matches, self)
     print('Match created with id: ' .. self.matchId)
     for _, player in ipairs(self.players) do
         playersMatch[player] = self.matchId
+        self:setPlayerData(player)
     end
 
     return self
+end
+
+function Match:endMatch()
+    for _, player in ipairs(self.players) do
+        self:resetPlayerData(player)
+        playersMatch[player] = nil
+    end
 end
 
 --- Retorna quantidade de partidas criadas
@@ -30,4 +39,37 @@ end
 --- @return boolean
 function Match.isPlayerInMatch(playerId)
     return playersMatch[playerId] ~= nil
+end
+
+--- Salva os dados do jogador
+--- @param playerId number
+function Match:setPlayerData(playerId)
+    self.playerData[playerId] = {}
+    local playerPed = NetworkGetEntityFromNetworkId(playerId)
+
+    local playerHealth = GetEntityHealth(playerPed)
+    local playerArmor = GetPedArmour(playerPed)
+
+    local playerCoords = GetEntityCoords(playerPed)
+    local playerHeading = GetEntityHeading(playerPed)
+    self.playerData[playerId] = {
+        health = playerHealth,
+        armour = playerArmor,
+        coords = playerCoords,
+        heading = playerHeading
+    }
+end
+
+--- Reseta os dados do jogador
+--- @param playerId number
+function Match:resetPlayerData(playerId)
+    local playerData = self.playerData[playerId]
+    if playerData then
+        local playerPed = NetworkGetEntityFromNetworkId(playerId)
+        SetEntityHealth(playerPed, playerData.health)
+        SetPedArmour(playerPed, playerData.armour)
+        SetEntityCoords(playerPed, playerData.coords[1], playerData.coords[2], playerData.coords[3], true, false, false,
+        false)
+        SetEntityHeading(playerPed, playerData.heading)
+    end
 end
--- server/main.lua
+++ server/main.lua
@@ -2,21 +2,22 @@ QueueManager = Queue.new()
 
 RegisterCommand('queue', function(source, args, rawCommand)
     local userId = source == 0 and args[1] or source
-
-    print('userId: ' .. userId)
-    print('isPlayerInMatch: ' .. (Match.isPlayerInMatch(userId) and 'true' or 'false'))
-    if Match.isPlayerInMatch(userId) then
+    local userPed = GetPlayerPed(userId)
+    local nId = NetworkGetNetworkIdFromEntity(userPed)
+    print('userId: ' .. nId)
+    print('isPlayerInMatch: ' .. (Match.isPlayerInMatch(nId) and 'true' or 'false'))
+    if Match.isPlayerInMatch(nId) then
         print('You are already in a match')
         return
     end
 
-    if QueueManager:isInQueue(userId) then
-        QueueManager:remove(userId)
+    if QueueManager:isInQueue(nId) then
+        QueueManager:remove(nId)
     else
-        QueueManager:add(userId)
+        QueueManager:add(nId)
     end
 
     print('queueCount: ' .. QueueManager:getQueueCount())
-    print('isInQueue: ' .. (QueueManager:isInQueue(userId) and 'true' or 'false'))
+    print('isInQueue: ' .. (QueueManager:isInQueue(nId) and 'true' or 'false'))
     print('matchesCount: ' .. Match:getMatchesCount())
 end, false)
