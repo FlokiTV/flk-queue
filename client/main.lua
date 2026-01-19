bf99750d1b2d8519730caf47cf0ca847f7d3840e
FlokiTV <dawysonw@gmail.com>
2026-01-19 07:39:07 -0300

    feat(queue): add client logging and improve server logging

--- client/main.lua
+++ client/main.lua
@@ -2,3 +2,7 @@ RegisterNetEvent('match:status', function(status)
     print('Queue count: ' .. status.queueCount)
     print('Matches count: ' .. status.matchesCount)
 end)
+
+RegisterNetEvent('queue:log', function(data)
+    print('[QUEUE]', data)
+end)
--- server/main.lua
+++ server/main.lua
@@ -1,13 +1,28 @@
 QueueManager = Queue.new()
 
+local function logClient(source, data)
+    if tonumber(source) == 0 then
+        print(data)
+    else
+        TriggerClientEvent('queue:log', source, data)
+    end
+end
+
 RegisterCommand('queue', function(source, args, rawCommand)
     local userId = source == 0 and args[1] or source
     local userPed = GetPlayerPed(userId)
+    logClient(source, 'userId: ' .. userId)
+    logClient(source, 'userPed: ' .. userPed)
+    if userPed == 0 then
+        logClient(source, 'Invalid player ped')
+        return
+    end
+
     local nId = NetworkGetNetworkIdFromEntity(userPed)
-    print('userId: ' .. nId)
-    print('isPlayerInMatch: ' .. (Match.isPlayerInMatch(nId) and 'true' or 'false'))
+    logClient(source, 'userNetId: ' .. nId)
+    logClient(source, 'isPlayerInMatch: ' .. (Match.isPlayerInMatch(nId) and 'true' or 'false'))
     if Match.isPlayerInMatch(nId) then
-        print('You are already in a match')
+        logClient(source, 'You are already in a match')
         return
     end
 
