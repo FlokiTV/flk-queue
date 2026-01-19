f58b8b0cff7d7e036bb7d26d1fc167be6fc5baab
FlokiTV <dawysonw@gmail.com>
2026-01-19 12:40:54 -0300

    perf(events): reduce win/lose message display time and wait interval

--- client/events/lose.lua
+++ client/events/lose.lua
@@ -2,12 +2,12 @@ local showLoser = false
 
 RegisterNetEvent(Event('lose'), function()
     showLoser = true
-    SetTimeout(5000, function()
+    SetTimeout(2500, function()
         showLoser = false
     end)
     Citizen.CreateThread(function()
         while showLoser do
-            Citizen.Wait(0)
+            Citizen.Wait(5)
             DrawTextCustom(0.5, 0.4, 'YOU LOSE!', 3.0,
                 { r = 255, g = 95, b = 95, a = 255 }, 4, true)
         end
--- client/events/win.lua
+++ client/events/win.lua
@@ -2,12 +2,12 @@ local showWinner = false
 
 RegisterNetEvent(Event('win'), function()
     showWinner = true
-    SetTimeout(5000, function()
+    SetTimeout(2500, function()
         showWinner = false
     end)
     Citizen.CreateThread(function()
         while showWinner do
-            Citizen.Wait(0)
+            Citizen.Wait(5)
             DrawTextCustom(0.5, 0.4, 'YOU WIN!', 3.0,
                 { r = 95, g = 255, b = 156, a = 255 }, 4, true)
         end
