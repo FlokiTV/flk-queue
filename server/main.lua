4ea78a892bf3a3a3e114e7556321f1bdae8b120d
FlokiTV <dawysonw@gmail.com>
2026-01-19 10:33:43 -0300

    feat(queue): add fade effects and routing buckets for match transitions
    - Implement fade in/out effects when starting matches for smoother transitions
    - Add player routing bucket assignment to isolate match instances
    - Clean up debug logs and add bucket inspection command

--- server/main.lua
+++ server/main.lua
@@ -2,31 +2,41 @@ QueueManager = Queue.new({
     maxPlayers = 2,
     onStart = function(match)
         print('Match created with id: ' .. match.matchId)
-        Log(-1, 'Match started with players: ' .. table.concat(match.players, ', '))
         local player1 = match.players[1]
         local player2 = match.players[2]
         local player1Ped = NetworkGetEntityFromNetworkId(player1)
+        local player1Source = NetworkGetEntityOwner(player1Ped)
         local player2Ped = NetworkGetEntityFromNetworkId(player2)
+        local player2Source = NetworkGetEntityOwner(player2Ped)
+        TriggerClientEvent('queue:fadeOut', player1Source, 500)
+        TriggerClientEvent('queue:fadeOut', player2Source, 500)
+        Citizen.Wait(500)
+        SetPlayerRoutingBucket(tostring(player1Source), match.matchId)
+        SetPlayerRoutingBucket(tostring(player2Source), match.matchId)
+        Citizen.Wait(200)
         SetEntityCoords(player1Ped, -1285.3945, -450.8088, 103.4655, true, false, false, true)
         SetEntityCoords(player2Ped, -1305.8623, -418.2008, 103.4656, true, false, false, true)
         SetEntityHeading(player1Ped, 34.0079)
         SetEntityHeading(player2Ped, 215.3134)
+        Citizen.Wait(500)
+        TriggerClientEvent('queue:fadeIn', player1Source, 500)
+        TriggerClientEvent('queue:fadeIn', player2Source, 500)
     end,
 })
 
 RegisterCommand('queue', function(source, args, rawCommand)
     local userId = source == 0 and args[1] or source
     local userPed = GetPlayerPed(userId)
-    Log(source, 'userId: ' .. userId)
-    Log(source, 'userPed: ' .. userPed)
+    -- Log(source, 'userId: ' .. userId)
+    -- Log(source, 'userPed: ' .. userPed)
     if userPed == 0 then
         Log(source, 'Invalid player ped')
         return
     end
 
     local nId = NetworkGetNetworkIdFromEntity(userPed)
-    Log(source, 'userNetId: ' .. nId)
-    Log(source, 'isPlayerInMatch: ' .. (Match.isPlayerInMatch(nId) and 'true' or 'false'))
+    -- Log(source, 'userNetId: ' .. nId)
+    -- Log(source, 'isPlayerInMatch: ' .. (Match.isPlayerInMatch(nId) and 'true' or 'false'))
     if Match.isPlayerInMatch(nId) then
         Log(source, 'You are already in a match')
         return
@@ -38,9 +48,15 @@ RegisterCommand('queue', function(source, args, rawCommand)
         QueueManager:add(nId)
     end
 
-    print('queueCount: ' .. QueueManager:getQueueCount())
-    print('isInQueue: ' .. (QueueManager:isInQueue(nId) and 'true' or 'false'))
-    print('matchesCount: ' .. Match:getMatchesCount())
+    -- print('queueCount: ' .. QueueManager:getQueueCount())
+    -- print('isInQueue: ' .. (QueueManager:isInQueue(nId) and 'true' or 'false'))
+    -- print('matchesCount: ' .. Match:getMatchesCount())
+end, false)
+
+RegisterCommand('bucket', function(source, args, rawCommand)
+    local userId = source == 0 and args[1] or source
+    local bucket = GetPlayerRoutingBucket(tostring(userId))
+    Log(source, 'bucket: ' .. bucket)
 end, false)
 
 CreateThread(function()
@@ -68,10 +84,10 @@ CreateThread(function()
                 local winner = players[1] == loser and players[2] or players[1]
                 local playerPed = NetworkGetEntityFromNetworkId(winner)
                 local playerSource = NetworkGetEntityOwner(playerPed)
-                print('Match ended ' .. match.matchId)
-                print('loser: ' .. loser)
-                print('winner: ' .. winner)
-                print('')
+                -- print('Match ended ' .. match.matchId)
+                -- print('loser: ' .. loser)
+                -- print('winner: ' .. winner)
+                -- print('')
                 Log(playerSource, 'You win!')
             end
         end
